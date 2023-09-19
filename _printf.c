70868118254a320516d8a9d52b28ab0ce25c5a4d
Ahmed maher <ahmedofficial5036@gmail.com>
2023-09-19 19:28:32 +0300

    Update _printf.c revert

--- _printf.c
+++ _printf.c
@@ -7,8 +7,8 @@
  */
 void _write_char(char c, int *count)
 {
-    write(1, &c, 1);
-    (*count)++;
+	write(1, &c, 1);
+	(*count)++;
 }
 
 /**
@@ -18,50 +18,14 @@ void _write_char(char c, int *count)
  */
 void _write_str(char *str, int *count)
 {
-    if (str == NULL)
-        str = "(null)";
+	if (str == NULL)
+		str = "(null)";
 
-    while (*str)
-    {
-        _write_char(*str, count);
-        str++;
-    }
-}
-
-/**
- * int_to_str - Converts an integer to a string
- * @num: The integer to convert
- *
- * Return: The resulting string
- */
-char *int_to_str(int num)
-{
-    char *str;
-    int num_copy = num;
-    int num_digits = 0;
-
-    /* Count the number of digits in the integer */
-    while (num_copy != 0)
-    {
-        num_copy /= 10;
-        num_digits++;
-    }
-
-    /* Allocate memory for the string (including space for '\0') */
-    str = malloc(num_digits + 1);
-    if (str == NULL)
-        return (NULL);
-
-    /* Build the string in reverse order */
-    str[num_digits] = '\0';
-    while (num != 0)
-    {
-        num_digits--;
-        str[num_digits] = '0' + (num % 10);
-        num /= 10;
-    }
-
-    return (str);
+	while (*str)
+	{
+		_write_char(*str, count);
+		str++;
+	}
 }
 
 /**
@@ -72,48 +36,38 @@ char *int_to_str(int num)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	va_list args;
+	int count = 0;
 
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format != '%')
-            _write_char(*format, &count);
-        else
-        {
-            format++;
-            switch (*format)
-            {
-            case 'c':
-                _write_char(va_arg(args, int), &count);
-                break;
-            case 's':
-                _write_str(va_arg(args, char *), &count);
-                break;
-            case '%':
-                _write_char('%', &count);
-                break;
-            case 'd':
-            case 'i':
-                {
-                    int num = va_arg(args, int);
-                    char *str = int_to_str(num);
-                    if (str != NULL)
-                    {
-                        _write_str(str, &count);
-                        free(str);
-                    }
-                }
-                break;
-            default:
-                _write_char('%', &count);
-                _write_char(*format, &count);
-                break;
-            }
-        }
-        format++;
-    }
-    va_end(args);
-    return (count);
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+			_write_char(*format, &count);
+		else
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				_write_char(va_arg(args, int), &count);
+				break;
+			case 's':
+				_write_str(va_arg(args, char *), &count);
+				break;
+			case '%':
+				_write_char('%', &count);
+				break;
+			default:
+				_write_char('%', &count);
+				_write_char(*format, &count);
+				break;
+			}
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
