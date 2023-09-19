fb44b080e97a8b9d60440ec74d8b7c6dd3296466
Ahmed maher <ahmedofficial5036@gmail.com>
2023-09-19 18:25:44 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -76,7 +76,6 @@ int _printf(const char *format, ...)
     int count = 0;
 
     va_start(args, format);
-
     while (*format)
     {
         if (*format != '%')
@@ -115,7 +114,6 @@ int _printf(const char *format, ...)
         }
         format++;
     }
-
     va_end(args);
     return (count);
 }
