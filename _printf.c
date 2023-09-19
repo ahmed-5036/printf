30213187ef96c5c2137b25f5858aa60f69a7208f
Ahmed maher <ahmedofficial5036@gmail.com>
2023-09-19 17:42:59 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,34 @@
 #include "main.h"
 
 /**
+ * _write_char - Write a single character to stdout
+ * @c: The character to be written
+ * @count: Pointer to the count of characters printed
+ */
+void _write_char(char c, int *count)
+{
+	write(1, &c, 1);
+	(*count)++;
+}
+
+/**
+ * _write_str - Write a string to stdout
+ * @str: The string to be written
+ * @count: Pointer to the count of characters printed
+ */
+void _write_str(char *str, int *count)
+{
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+	{
+		_write_char(*str, count);
+		str++;
+	}
+}
+
+/**
  * _printf - Custom printf function
  * @format: Format string
  *
@@ -8,56 +36,38 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;     /* Declare a variable to handle variable arguments */
-    int count = 0;    /* Initialize a counter for characters printed */
-    char c;           /* Temporary variable for characters */
-    char *str;        /* Temporary variable for strings */
+	va_list args;
+	int count = 0;
 
-    va_start(args, format); /* Initialize the va_list to work with arguments */
+	va_start(args, format);
 
-    while (*format) /* Loop through each character in the format string */
-    {
-        if (*format != '%') /* If it's not a '%', print it and increment the count */
-        {
-            write(1, format, 1);
-            count++;
-        }
-        else
-        {
-            format++; /* Move to the next character after '%' */
-            switch (*format)
-            {
-                case 'c':
-                    c = va_arg(args, int); /* Extract a character argument */
-                    write(1, &c, 1); /* Write the character */
-                    count++; /* Increment the count */
-                    break;
-                case 's':
-                    str = va_arg(args, char *); /* Extract a string argument */
-                    if (str == NULL)
-                        str = "(null)"; /* Handle NULL strings */
-                    while (*str)
-                    {
-                        write(1, str, 1); /* Write each character of the string */
-                        str++;
-                        count++;
-                    }
-                    break;
-                case '%':
-                    write(1, "%", 1); /* Write a '%' character */
-                    count++;
-                    break;
-                default:
-                    /* Handle unsupported specifier, write both '%' and the character */
-                    write(1, &format[-1], 1);
-                    write(1, format, 1);
-                    count += 2;
-                    break;
-            }
-        }
-        format++; /* Move to the next character in the format string */
-    }
+	while (*format)
+	{
+		if (*format != '%')
+			_write_char(*format, &count);
+		else
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				_write_char(va_arg(args, int), &count);
+				break;
+			case 's':
+				_write_str(va_arg(args, char *), &count);
+				break;
+			case '%':
+				_write_char('%', &count);
+				break;
+			default:
+				_write_char('%', &count);
+				_write_char(*format, &count);
+				break;
+			}
+		}
+		format++;
+	}
 
-    va_end(args); /* Clean up the va_list */
-    return (count); /* Return the total count of characters printed */
+	va_end(args);
+	return (count);
 }
